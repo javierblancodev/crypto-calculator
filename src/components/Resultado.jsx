c45fbe9ae5fdadfd84bebcfd784dc64817d5039b
Javier Blanco <javierblancoit@gmail.com>
2022-07-06 11:04:39 +0100

    add style to quotation display

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,17 +1,34 @@
 import styled from "@emotion/styled";
 
+const Cotizacion = styled.div`
+    color: #ffffff;
+    font-family: 'Lato', sans-serif;
+`;
 
+const Texto = styled.p`
+    font-size: 18px;
+    span {
+        font-weight: 700;
+    }
+`;
+
+const Precio = styled.p`
+    font-size: 30px;
+    span {
+        font-weight: 700;
+    }
+`;
 
 const Resultado = ({resultado}) => {
     const {PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE} = resultado;
     return (
-        <div>
-            <p>Current Price: <span>{PRICE}</span></p>
-            <p>Highest Value: <span>{HIGHDAY}</span></p>
-            <p>Lowest Value: <span>{LOWDAY}</span></p>
-            <p>Change Last 24h (%): <span>{CHANGEPCT24HOUR}</span></p>
-            <p>Last Update: <span>{LASTUPDATE}</span></p>
-        </div>
+        <Cotizacion>
+            <Precio>Current Price: <span>{PRICE}</span></Precio>
+            <Texto>Highest Value: <span>{HIGHDAY}</span></Texto>
+            <Texto>Lowest Value: <span>{LOWDAY}</span></Texto>
+            <Texto>Change Last 24h: <span>{CHANGEPCT24HOUR}%</span></Texto>
+            <Texto>Last Update: <span>{LASTUPDATE}</span></Texto>
+        </Cotizacion>
     )
 }
 
