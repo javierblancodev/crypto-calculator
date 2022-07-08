9c00ae4036da96aa3bb3f5d7221f1ff55be3159f
Javier Blanco <javierblancoit@gmail.com>
2022-07-08 11:28:00 +0100

    add crypto image to the Resultado component

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -3,7 +3,17 @@ import styled from "@emotion/styled";
 const Cotizacion = styled.div`
     color: #ffffff;
     font-family: 'Lato', sans-serif;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    gap: 1rem;
+    margin-top: 30px;
 `;
+
+const Imagen = styled.img`
+    display: block;
+    width: 90px;
+`
 
 const Texto = styled.p`
     font-size: 18px;
@@ -13,7 +23,7 @@ const Texto = styled.p`
 `;
 
 const Precio = styled.p`
-    font-size: 30px;
+    font-size: 22px;
     span {
         font-weight: 700;
     }
@@ -23,11 +33,12 @@ const Resultado = ({resultado}) => {
     const {PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE} = resultado;
     return (
         <Cotizacion>
-            <Precio>Current Price: <span>{PRICE}</span></Precio>
+            <Imagen src={`https://www.cryptocompare.com/${IMAGEURL}`} alt="crypto" />
+            <div><Precio>Current Price: <span>{PRICE}</span></Precio>
             <Texto>Highest Value: <span>{HIGHDAY}</span></Texto>
             <Texto>Lowest Value: <span>{LOWDAY}</span></Texto>
             <Texto>Change Last 24h: <span>{CHANGEPCT24HOUR}%</span></Texto>
-            <Texto>Last Update: <span>{LASTUPDATE}</span></Texto>
+            <Texto>Last Update: <span>{LASTUPDATE}</span></Texto></div>
         </Cotizacion>
     )
 }
